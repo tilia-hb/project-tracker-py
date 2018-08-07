b0b56f5935edd3851d283be7d1ffcedfff9fb86d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-08-06 20:58:04 -0700

    completed get_project_by_title function and call handle

--- hackbright.py
+++ hackbright.py
@@ -59,7 +59,18 @@ def make_new_student(first_name, last_name, github):
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+
+    QUERY = """
+        SELECT *
+        FROM projects
+        WHERE title = :title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'title': title})
+
+    row = db_cursor.fetchone()
+
+    print("Project: {} \n info: {} \nMax Grade: {}".format(row[1], row[2], row[3]))
 
 
 def get_grade_by_github_title(github, title):
@@ -94,6 +105,12 @@ def handle_input():
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
+
+        elif command == "project_info":
+            title = args[0]
+            get_project_by_title(title)
+
+
 
         else:
             if command != "quit":
